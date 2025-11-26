ef310b9072851b4f4a7c68dca61868a0665d7346
Raphael Castelar <castelaraphael@gmail.com>
2025-11-26 11:43:27 -0300

    specific adjustments on app and assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/signup/signup.component';
 
 export const routes: Routes = [
-    
     {
-        path: 'login',
+        path: "login",
         component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignUpComponent
     }
-
 ];
